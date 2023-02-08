f59d6b45460507e455850bf4b2b5d83d916ddec0
kingkhan-abd <eijazking6791@gmail.com>
2023-02-08 23:27:44 +0500

    my website

---
+++ app.js
@@ -0,0 +1,99 @@
+// Question no:1
+var username;
+
+// Question no:2
+var myName = "Eijaz khan";
+
+// Question no:3
+var message = "Hello World";
+alert(message);
+
+// Question no:4
+var name = "Jhone Doe";
+alert(name);
+var age = "15 years old";
+alert(age);
+var skills = "Certified Moblie Application Development";
+alert(skills);
+
+// Question no:5
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(pizza);
+
+// Question no:6
+var email = "example@example.com";
+alert("My email address is" + email);
+
+// Question no:7
+var book = "A smarter way to learn JavaScript";
+alert(book);
+
+
+// Question no:8
+document.write("Yah!I can write HTML content though JavaScript");
+
+// Question no:9
+var symbol = "✿ஜீ۞ஜீ✿•.¸¸.•*`*•.•ஜீ☼۞";
+alert(symbol);
+
+
+
+
+
+// CHAPTER 03
+
+// Question no:1
+var age = "I am 22 years old";
+alert(age);
+
+// Questin no:2
+// var noOfVisitingTimes = "You have visited 14 times to our site thanks for your visit\n";
+// alert(noOfVisitingTimes);
+
+// Question no:3
+var birthYear = "My birth year is  2000";
+document.write(birthYear);
+
+// Question no:4
+var visitorName = "Eijaz khan\n";
+var Quantity = "5-Tshirt";
+var poductTitle = "Tshirt";
+document.write(visitorName +"Ordered"+Quantity+ "from Online Shopping Store");
+
+
+
+
+// CHAPTER 04
+
+// Question no:1
+var name,age,email;
+
+//Question no:2
+// 5 illegal names of variable
+// var 1stname;
+// var -time ;
+// var @newvariale;
+// var birthyear;
+// var var;
+
+
+// 5 legal names of variable
+var _name;
+var birthYear;
+var quan1ty;
+var $dollar;
+var $dollar$;
+
+// Question no:3
+document.write("variables names can only contain numbers $, _Forexample$myvariable\n");
+document.write("variable name begin with a letter ,$, For example $name,_name or name\n");
+document.write("variables names are case sensitive\n");
+document.write("Variables names should not be Js keywords\n");
+
+
+
+
+
+
+
+
